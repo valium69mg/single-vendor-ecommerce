5bd2ec34c65265491adf2b5ae9270004d339f6e6
valium69mg <xdeus.v2@gmail.com>
2026-02-04 19:10:55 -0600

    feat: batman

---
+++ src/main/java/com/croman/SingleVendorEcommerce/SingleVendorEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.croman.SingleVendorEcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleVendorEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleVendorEcommerceApplication.class, args);
+	}
+
+}
