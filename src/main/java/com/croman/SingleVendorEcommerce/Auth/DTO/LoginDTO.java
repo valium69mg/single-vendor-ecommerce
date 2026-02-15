aa327c63ac366caff04b938a213bc2620b299345
valium69mg <xdeus.v2@gmail.com>
2026-02-14 18:51:05 -0600

    feat: eliminar regex de password en login

--- src/main/java/com/croman/SingleVendorEcommerce/Auth/DTO/LoginDTO.java
+++ src/main/java/com/croman/SingleVendorEcommerce/Auth/DTO/LoginDTO.java
@@ -2,7 +2,6 @@ package com.croman.SingleVendorEcommerce.Auth.DTO;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,7 +15,6 @@ public class LoginDTO {
 	@Email(message = "Email must be valid")
 	private String email;
 	@NotBlank(message = "Password is required")
-	@Size(min = 8, max = 64, message = "Password must be between 8 and 64 characters")
 	private String password;
 	
 }
