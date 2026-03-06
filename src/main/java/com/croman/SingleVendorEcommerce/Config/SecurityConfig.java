558c45811d929c4e7b1a5348018c2dda12fecc9d
carlostr <croman@ibl.mx>
2026-03-06 14:31:00 -0600

    feat: agregar sessionManagement to SecurityConfig

--- src/main/java/com/croman/SingleVendorEcommerce/Config/SecurityConfig.java
+++ src/main/java/com/croman/SingleVendorEcommerce/Config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.croman.SingleVendorEcommerce.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -24,6 +25,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers(SWAGGER_WHITELIST)
 						.permitAll()
