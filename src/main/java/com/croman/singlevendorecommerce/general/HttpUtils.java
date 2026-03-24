040eff236bd0cd00407dee81e30d2d6b8d6ceea2
carlostr <xdeus.v2@gmail.com>
2026-03-24 08:58:23 -0600

    fix: arreglar issue de sonar en HttpUtils 'Utility classes should not have public constructorsjava:S1118'

--- src/main/java/com/croman/singlevendorecommerce/general/HttpUtils.java
+++ src/main/java/com/croman/singlevendorecommerce/general/HttpUtils.java
@@ -2,8 +2,12 @@ package com.croman.singlevendorecommerce.general;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-public class HttpUtils {
+public final class HttpUtils {
 
+	private HttpUtils() {
+		
+	}
+	
 	public static String getClientIp(HttpServletRequest request) {
 	    String ip = request.getHeader("X-Forwarded-For");
 	    if (ip != null && !ip.isEmpty()) {
