53f1ff365557a4413315876b5680437f9eb800cb
carlostr <xdeus.v2@gmail.com>
2026-03-24 08:55:25 -0600

    fix: arreglar issue de sonar Generic exceptions should never be thrownjava:S112

--- src/main/java/com/croman/singlevendorecommerce/config/SecurityConfig.java
+++ src/main/java/com/croman/singlevendorecommerce/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain filterChain(HttpSecurity http) {
 		http.csrf(csrf -> csrf.disable())
 				.cors(cors -> {})
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
