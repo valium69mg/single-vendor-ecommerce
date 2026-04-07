5187f2c3e33ede809e2cb4f70cd360a6184e8194
carlostr <xdeus.v2@gmail.com>
2026-04-07 10:40:51 -0600

    feat: agregar variable de entorno CORS_ORIGIN para dictar el origden de cors

--- src/main/java/com/croman/singlevendorecommerce/config/SecurityConfig.java
+++ src/main/java/com/croman/singlevendorecommerce/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers(SWAGGER_WHITELIST)
 						.permitAll()
-						.requestMatchers("/health", "/health/**")
+						.requestMatchers("/health")
 						.permitAll()
 						.requestMatchers("/api/v1/auth/login", "/api/v1/users/register", "/api/v1/users/register/admin")
 						.permitAll()
